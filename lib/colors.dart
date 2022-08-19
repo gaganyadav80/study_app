7756c1fa42d4b095743e345dccacd3dae9916d37
gaganyadav80 <gaganyadav80@gmail.com>
2022-08-19 16:46:50 +0530

    first commit

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kAccentColor = Color(0xff7C5EFB);
+const Color kMasteredTopicButtonColor = Color(0xff0D3390);
+const Color kWeakTopicButtonColor = Color(0xffE4534B);
+const Color kUncoveredTopicButtonColor = Color(0xff6C8291);
---
+++ lib/constants.dart
@@ -0,0 +1,35 @@
+enum ListType { mastered, weak, uncovered }
+
+const List<String> masteredTopicsList = [
+  'Introduction to the Constitution',
+  'The Union & Its Territories',
+  'Directive Principles of State Policy',
+  'State Council of Ministers',
+  'Introduction to the Constitution',
+  'The Union & Its Territories',
+  'Directive Principles of State Policy',
+  'State Council of Ministers',
+  'Introduction to the Constitution',
+  'The Union & Its Territories',
+  'Directive Principles of State Policy',
+  'State Council of Ministers',
+];
+const List<String> weakTopicsList = [
+  'Introduction to the Constitution',
+  'The Union & Its Territories',
+  'Directive Principles of State Policy',
+  'State Council of Ministers',
+  'Introduction to the Constitution',
+  'The Union & Its Territories',
+  'Directive Principles of State Policy',
+  'State Council of Ministers',
+  'Introduction to the Constitution',
+  'The Union & Its Territories'
+];
+const List<String> uncoveredTopicsList = [
+  'Introduction to the Constitution',
+  'The Union & Its Territories',
+  'Directive Principles of State Policy',
+  'State Council of Ministers',
+  'Introduction to the Constitution'
+];
